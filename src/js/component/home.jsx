90aa6ff5c81f63e4c84b19397f9230e68a3029b5
jsosa26 <99664039+jsosa26@users.noreply.github.com>
2023-09-11 23:57:24 +0000

    primeras constantes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,22 +5,9 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
-export default Home;
+	const rojoEnc ="rojoEnc";
+	const amarilloEnc ="amarilloEnc";
+	const verdeEnc ="verdeEnc";
+	const rojoApag ="rojoApag";
+	const amarilloApag ="amarilloApag";
+	const verdeApag ="verdeApag";
