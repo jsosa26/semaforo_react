56ae3794dadf83f85a7c4f0e6ef8883453577834
jsosa26 <99664039+jsosa26@users.noreply.github.com>
2023-09-12 00:14:14 +0000

    react listo, llamadas y todo listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,3 +11,21 @@ const Home = () => {
 	const rojoApag ="rojoApag";
 	const amarilloApag ="amarilloApag";
 	const verdeApag ="verdeApag";
+
+	const [luzRoja, setLightRed] = useState(rojoApag);
+	const [luzAmarilla, setLightYellow] = useState(amarilloApag);
+	const [luzVerde, setLightGreen] = useState(verdeApag);
+
+		return (
+			<div className="main-content">
+			<h1 className="text-center mt-5">El gran Semaforo</h1>
+				<div className="cuerpo">
+					<div className={luzRoja} onClick={()=>setLightRed(rojoEnc) + setLightYellow(amarilloApag) + setLightGreen(verdeApag)}></div>
+					<div className={luzAmarilla} onClick={()=>setLightYellow(amarilloEnc) + setLightRed(rojoApag) + setLightGreen(verdeApag)}></div>
+					<div className={luzVerde} onClick={()=>setLightGreen(verdeEnc) + setLightYellow(amarilloApag) + setLightRed(rojoApag)}></div>				
+				</div>
+			</div>
+		);
+};
+
+export default Home;
